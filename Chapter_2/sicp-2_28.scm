6cf7c83d6c1464e1576bb4c6fee8d0d1538635d4
mput <putintsev@gmail.com>
2017-10-17 19:48:17 +0300

    Add exc 2_28! Yep!

--- Chapter_2/sicp-2_28.scm
+++ Chapter_2/sicp-2_28.scm
@@ -2,9 +2,13 @@
 (use sicp)
 (define nil (quote ()))
 
-(define x (list (list 1 2) (list 3 4)))
+(define x (list (list 1 2 (list 9 10 11)) (list 3 4) (list 5 6)))
 
-(define (fringe list)
-  (cond ((null? list) list)
-        ((pair? list) (car list))
-        (else (cons list))))
+(define (fring list)
+  (print "----->" list)
+  (cond ((null? list) nil)
+        ((not (pair? list)) (cons list nil))
+        (else (append (fring (car list)) (fring (cdr list))))))
+
+(print (fring x))
+
