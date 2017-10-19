009ae3228f4eb0a4a1f450ca91be7b246b7f9b4e
mput <putintsev@gmail.com>
2017-10-19 23:32:15 +0300

    Start 2_42

---
+++ Chapter_2/sicp-2_42.scm
@@ -0,0 +1,30 @@
+#! /usr/local/bin/csi -s
+(use sicp)
+(define nil (quote ()))
+;;Not ready yet (
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+          (lambda (position) (safe? k position))
+          (flatmap
+            (lambda (rest-of-queens)
+              (map (lambda (new-row)
+                      (adjoin-position new-row rest-of-queens))
+                (enumerate-interval 1 board-size)))
+            (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+(define empty-board nil)
+
+(define (enumerate-interval start finish)
+  (if (> start finish)
+      nil
+      (cons start (enumerate-interval (+ start 1) finish))))
+
+(define (flatmap fun seq)
+  (accumulate append nil (map fun seq)))
+
+
+(print (queens 5))
