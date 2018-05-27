0811dc4275267dbe0e35df157a0285c6a7503421
mput <putintsev@gmail.com>
2018-05-27 22:53:01 +0300

    Moved old exercise solution to old dir

--- Chapter_1/sicp-1_41.scm
+++
@@ -1,8 +0,0 @@
-#! /usr/local/bin/csi -s
-(use sicp)
-
-(define (double fun) (lambda (x) (fun (fun x))))
-
-(print ((double (lambda (x) (+ 1 x))) 5))
-
-(print (((double (double double)) (lambda (x) (+ 1 x))) 5))
