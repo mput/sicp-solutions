04622f8ccfb8685728b126f1cf9beaf7371953f9
mput <putintsev@gmail.com>
2018-06-28 19:49:10 +0300

    Some lenting

--- tests/1_07.test.rkt
+++ tests/1_07.test.rkt
@@ -11,14 +11,14 @@
     "Test my-sqrt function"
     (test-case
       "Normal numbers"
-          (check-true (aprox-equal? (my-sqrt 9) (sqrt 9)))
-          (check-true (aprox-equal? (square (my-sqrt 15)) 15)))
+      (check-true (aprox-equal? (my-sqrt 9) (sqrt 9)))
+      (check-true (aprox-equal? (square (my-sqrt 15)) 15)))
     (test-case
       "Small numbers"
-            (check-true (aprox-equal? (my-sqrt 0.8) (sqrt 0.8))))
+      (check-true (aprox-equal? (my-sqrt 0.8) (sqrt 0.8))))
     (test-case
       "Case here"
-            (check-true (aprox-equal? (my-sqrt 400) (sqrt 400))))))
+      (check-true (aprox-equal? (my-sqrt 400) (sqrt 400))))))
 
 (run-tests tests 'verbose)
 
