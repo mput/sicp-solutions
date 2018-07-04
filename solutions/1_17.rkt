f626ebc8729de2fc19fd288daa0d79f6ba6882d1
mput <putintsev@gmail.com>
2018-07-04 13:21:31 +0300

    Add iterative form for solution.

--- solutions/1_17.rkt
+++ solutions/1_17.rkt
@@ -2,13 +2,19 @@
 #| Solution for exercise 1_17. |#
 
 (require rackunit "../solutions/utils.rkt")
-(provide fast-mult)
+(provide fast-mult-recrs fast-mult-iter)
 
 (define (double a) (+ a a))
 (define (halve a) (/ a 2))
 
-(define (fast-mult a b)
+(define (fast-mult-recrs a b)
   (cond ((= b 0) 0)
-        ((even? b) (double (fast-mult a (halve b))))
-        (else (+ a (fast-mult a (- b 1))))))
+        ((even? b) (double (fast-mult-recrs a (halve b))))
+        (else (+ a (fast-mult-recrs a (- b 1))))))
 
+(define (fast-mult-iter a b)
+  (define (helper-iter acc a b)
+    (cond ((= b 0) acc)
+          ((even? b) (helper-iter acc (double a) (halve b)))
+          (else (helper-iter (+ acc a) a (- b 1)))))
+  (helper-iter 0 a b))
--- tests/1_17.test.rkt
+++ tests/1_17.test.rkt
@@ -4,14 +4,15 @@
 (require rackunit "../solutions/1_17.rkt")
 
 
-(define tests
+(define (tests proc)
   (test-suite
     "simple suite"
-    (check-equal? (fast-mult 3 5) (* 3 5) "Simple mult")
-    (check-equal? (fast-mult 123 13423) (* 123 13423))
-    (check-equal? (fast-mult 5 123456789) (* 5 123456789))
-    (check-equal? (fast-mult 0 0) 0 "Zero case")
-    (check-equal? (fast-mult 23 1) (* 23 1) "Mult to one case")))
+    (check-equal? (proc 3 5) (* 3 5) "Simple mult")
+    (check-equal? (proc 123 13423) (* 123 13423))
+    (check-equal? (proc 5 123456789) (* 5 123456789))
+    (check-equal? (proc 0 0) 0 "Zero case")
+    (check-equal? (proc 23 1) (* 23 1) "Mult to one case")))
 
-(run-tests tests 'verbose)
+(run-tests (tests fast-mult-recrs) 'verbose)
+(run-tests (tests fast-mult-iter) 'verbose)
 
