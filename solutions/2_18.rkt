ca88a6a127ad7cb340de560635cd5164bc55d1bb
mput <putintsev@gmail.com>
2018-08-16 00:46:36 +0300

    Added iterative sulution

--- solutions/2_18.rkt
+++ solutions/2_18.rkt
@@ -2,7 +2,7 @@
 #| Solution for exercise 2_18. |#
 
 (require rackunit "../solutions/utils.rkt")
-(provide reverse)
+(provide reverse reverse-iter)
 
 (define (append list1 list2)
   (if (null? list1)
@@ -14,3 +14,9 @@
     items
     (append (reverse (cdr items)) (list (car items)))))
 
+(define (reverse-iter items)
+  (define (helper acc rest)
+    (if  (null? rest)
+      acc
+      (helper (cons (car rest) acc) (cdr rest))))
+  (helper '() items))
--- tests/2_18.test.rkt
+++ tests/2_18.test.rkt
@@ -7,7 +7,8 @@
 (define tests
   (test-suite
     "Test for exercise 2_18"
-    (check-equal? (reverse (list 1 2 3 4 5)) (list 5 4 3 2 1) "Should return reverse list")))
+    (check-equal? (reverse (list 1 2 3 4 5)) (list 5 4 3 2 1) "Should return reverse list")
+    (check-equal? (reverse-iter (list 1 2 3 4 5)) (list 5 4 3 2 1) "Should return reverse list")))
 
 (run-tests tests 'verbose)
 
