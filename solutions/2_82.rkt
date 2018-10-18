d80b86f9321a111464979893197d1f37b79e256a
mput <putintsev@gmail.com>
2018-10-18 16:57:01 +0300

    Fixing and refactoring

--- solutions/2_82.rkt
+++ solutions/2_82.rkt
@@ -2,71 +2,39 @@
 #| Solution for exercise 2_82. |#
 
 (require (only-in "../solutions/dispatch-table.rkt" type-tag contents get put get-coercion put-coercion))
-(provide apply-generic-coercion install-equ?-with-three-args-package)
+(provide apply-generic-coercion)
 
 (define (every? items)
   (cond ((null? items) true)
         ((not (car items)) false)
-        (every? (cdr items))))
+        (else (every? (cdr items)))))
 
-(define (try-coerce from-type to-type arg)
-  (if (eq? from-type to-type)
-    arg
-    (let ((coersion (get-coercion from-type to-type)))
-      (if coersion
-        (coersion arg)
-        false))))
+(define (get-types args) (map type-tag args))
+(define (get-contents args) (map contents args))
 
-
-(define (apply-generic op args)
-  (let ((types (map type-tag args))
-        (contents (map contents args)))
-    (let ((proc (get op types)))
-      (if proc
-        (apply proc contents)
-        false))))
-
-
-(define (try-apply-with-coercion op args)
-  (define (try-coerce-and-apply to-type-arg from-type-args)
-    (let ((coercioned-args (map (lambda (from-arg) (try-coerce (type-tag from-arg) (type-tag to-type-arg) from-arg)) from-type-args)))
-      (if (every? coercioned-args)
-        (let ((applyed-proc (apply-generic op (cons to-type-arg coercioned-args))))
-          (if applyed-proc
-            applyed-proc
-            false))
-        false)))
-
-  (define (helper tried not-tried)
-    (if (null? not-tried)
-      false
-      (let ((to-type-arg (car not-tried))
-            (from-type-args (append tried (cdr not-tried))))
-        (let ((applyed-proc (try-coerce-and-apply to-type-arg from-type-args)))
-          (if applyed-proc
-            applyed-proc
-            (helper (append tried (list to-type-arg)) (cdr not-tried)))))))
-  (helper '() args))
+(define (try-coerce arg to-type)
+  (let ((from-type (type-tag arg)))
+    (if (eq? from-type to-type)
+      arg
+      (let ((coersion (get-coercion from-type to-type)))
+        (if coersion
+          (coersion arg)
+          false)))))
 
 (define (apply-generic-coercion op . args)
-  (let ((applyed-without-coercion (apply-generic op args)))
-    (if applyed-without-coercion
-      applyed-without-coercion
-      (let ((applyed-with-coercion (try-apply-with-coercion op args)))
-        (if applyed-with-coercion
-          applyed-with-coercion
-          (error "No method for these types"
-                 (list op args)))))))
+  (define (try-coerce-apply-to-first rest-args)
+    (if (null? rest-args)
+      (error "No method for these types"
+             (list op (type-tag args)))
+      (let ((to-type (type-tag (car rest-args))))
+        (let ((coerced (map (lambda (arg) (try-coerce arg to-type)) args)))
+          (if (every? coerced)
+            (let ((proc (get op (get-types coerced))))
+              (if proc
+                (apply proc (get-contents coerced))
+                (try-coerce-apply-to-first (cdr rest-args))))
+            (try-coerce-apply-to-first (cdr rest-args)))))))
+  (try-coerce-apply-to-first args))
 
 
-(define (install-equ?-with-three-args-package)
-  (define real-part (get 'real-part '(complex)))
-  (define imag-part (get 'imag-part '(complex)))
-  (define (equ? x y z) (apply-generic-coercion 'equ? x y z))
-  (put 'equ? '(complex complex complex)
-       (lambda (x y z)
-         (and (equ? (real-part x) (real-part y) (real-part z))
-              (equ? (imag-part x) (imag-part y) (imag-part z)))))
-  (put 'equ? '(scheme-number scheme-number scheme-number)
-       (lambda (x y z) (= x y z))))
 
--- tests/2_82.test.rkt
+++ tests/2_82.test.rkt
@@ -5,7 +5,18 @@
 (require (only-in "../solutions/dispatch-table.rkt" get put))
 (require (only-in "../solutions/2_79.rkt" install-equ?-package))
 (require (only-in "../solutions/2_81.rkt" install-coercion-package))
-(require (only-in "../solutions/2_82.rkt" apply-generic-coercion install-equ?-with-three-args-package))
+(require (only-in "../solutions/2_82.rkt" apply-generic-coercion))
+
+(define (install-equ?-with-three-args-package)
+  (define real-part (get 'real-part '(complex)))
+  (define imag-part (get 'imag-part '(complex)))
+  (define (equ? x y z) (apply-generic-coercion 'equ? x y z))
+  (put 'equ? '(complex complex complex)
+       (lambda (x y z)
+         (and (equ? (real-part x) (real-part y) (real-part z))
+              (equ? (imag-part x) (imag-part y) (imag-part z)))))
+  (put 'equ? '(scheme-number scheme-number scheme-number)
+       (lambda (x y z) (= x y z))))
 
 (install-scheme-number-package)
 (install-rational-package)
@@ -16,22 +27,32 @@
 
 (define (make-complex-from-real-imag x y)
   ((get 'make-from-real-imag 'complex) x y))
-(define (make-complex-from-mag-ang r a)
-  ((get 'make-from-mag-ang 'complex) r a))
+(define (make-rational n d)
+  ((get 'make 'rational) n d))
 
 (define (equ? x y z) (apply-generic-coercion 'equ? x y z))
+
+(define compl-num (make-complex-from-real-imag 3 0))
+(define compl-num2 (make-complex-from-real-imag 4 0))
+(define rat-num (make-rational 3 1))
 
 (define tests
   (test-suite
     "Test for exercise 2_82"
     (test-case
-      "Test coersions two scheme-number->complex"
-      (define compl-num (make-complex-from-real-imag 3 0))
+      "Test coersions three scheme-number->complex"
       (check-true (equ? 3 3 3))
       (check-true (equ? compl-num compl-num compl-num))
       (check-true (equ? compl-num 3 3))
-      (check-true (equ? 3 compl-num 3))
-      (check-true (equ? 3 compl-num compl-num)))))
+      (check-true (equ? 3 compl-num 3)))
+    (test-case
+      "Falsy cases with three scheme-number->complex"
+      (check-false (equ? 3 compl-num 4))
+      (check-false (equ? 3 compl-num compl-num2))
+      (check-false (equ? compl-num compl-num2 compl-num)))
+    (test-case
+      "Should throw on type for which we didn't have coersions"
+      (check-exn exn:fail? (lambda () (equ? rat-num 3 3))))))
 
 (run-tests tests 'verbose)
 
