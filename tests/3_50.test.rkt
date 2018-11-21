0fd55662bfe4a13906a0172e6f47c779ff5104ae
mput <putintsev@gmail.com>
2018-11-21 20:30:34 +0300

    Add exc 3_50

--- tests/3_50.test.rkt
+++ tests/3_50.test.rkt
@@ -2,21 +2,19 @@
 (require racket/stream)
 
 (require rackunit  rackunit/text-ui)
-(require (only-in "../solutions/3_50.rkt" stream-map-my))
+(require (only-in "../solutions/3_50.rkt" streams-map))
 
 (define my-stream (stream 1 2 3 4 5))
+(define my-stream2 (stream 10 20 30 40 50))
 
-(define x (stream-first (stream-rest (stream-map (lambda (x) (* x x)) my-stream))))
 
-(displayln x)
 
-;; (define tests
-;;   (test-suite
-;;     "Test for exercise 3_50"
-;;     (check-equal? solutions 1 "")
-;;     (test-case
-;;       "Case here"
-;;       (check-equal? solutions 0 ""))))
+(define tests
+  (test-suite
+    "Test for exercise 3_50"
+    (test-case
+      "Case here"
+      (define x (stream-first (stream-rest (streams-map (lambda (x y) (* x y)) my-stream my-stream2))))
+      (check-equal? x 40))))
 
-;; (run-tests tests 'verbose)
-
+(run-tests tests 'verbose)
